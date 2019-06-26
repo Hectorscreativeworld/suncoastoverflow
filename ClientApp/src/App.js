984e45fa60a3bf1217821a7f88b138d4d5437a5a
hectorscreativeworld <hectorscreativeworld@gmail.com>
2019-06-26 14:02:42 -0400

    AnsweredHomePage

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router'
 import { Layout } from './components/Layout'
+import AnsweredHomePage from './components/Pages/AnsweredHomePage'
+import AskAQuestionHomePage from './components/Pages/AskAQuestionHomePage'
+
 import Home from './components/Pages/Home'
 
 export default class App extends Component {
@@ -10,6 +13,12 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path="/" component={Home} />
+        <Route exact path="AnsweredHomePage" component={AnsweredHomePage} />
+        <Route
+          exact
+          path="AskAQuestionHomePage"
+          component={AskAQuestionHomePage}
+        />
       </Layout>
     )
   }
---
+++ ClientApp/src/components/Pages/AnsweredHomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AnsweredHomePage extends Component {
+  render() {
+    return (
+      <div>
+        <span class="d-block p-2 bg-primary text-white">d-block</span>
+      </div>
+    )
+  }
+}
+
+export default AnsweredHomePage
