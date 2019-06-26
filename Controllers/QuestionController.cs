8eadbc01423851364bff738ad8144f8a59a5f1c3
dantezinfern0 <danteharasz@gmail.com>
2019-06-26 09:26:41 -0400

    first endpoint

--- Controllers/QuestionController.cs
+++ Controllers/QuestionController.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using suncoastoverflow.Models;
 
 namespace suncoastoverflow.Controllers
 {
   [Route("api/[controller]")]
-   public class Controller : Controller
+  public class SampleDataController : Controller
   {
+    [Route("api/[controller]")]
+    public class QuestionController : Controller
+    {
+      [HttpGet]
+      public ActionResult<List<Question>> GetUnansweredQuestions()
+      {
+        var db = new DatabaseContext();
+        var all = db.QuestionsTable.Where(w => w.Answered == false);
+        return all.ToList();
+      }
+
+    }
+  }
