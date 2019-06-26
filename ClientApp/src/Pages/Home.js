b233e105fd30a4d77ae3383f28c444f553c7e124
dantezinfern0 <danteharasz@gmail.com>
2019-06-26 14:57:35 -0400

    dir change

--- ClientApp/src/Pages/Home.js
+++ ClientApp/src/Pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Questions from '../Questions'
+import Questions from '../components/Questions'
 import TopQuestions from './TopQuestions'
 
 class Home extends Component {
