76fe5ea80e7766abb7c25fc7f3697c24a522d708
hectorscreativeworld <hectorscreativeworld@gmail.com>
2019-06-26 14:35:48 -0400

    fixing issues

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router'
 import { Layout } from './components/Layout'
-import AnsweredHomePage from './components/Pages/AnsweredHomePage'
-import AskAQuestionHomePage from './components/Pages/AskAQuestionHomePage'
+import AnsweredHomePage from './Pages/AnsweredHomePage'
+import AskAQuestionHomePage from './Pages/AskAQuestionHomePage'
 
-import Home from './components/Pages/Home'
+import Home from './Pages/Home'
 
 export default class App extends Component {
   static displayName = App.name
