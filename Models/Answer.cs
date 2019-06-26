fd5d71cf0c8906ed41e66de9ce5b562bcca5fc8b
dantezinfern0 <danteharasz@gmail.com>
2019-06-25 21:48:53 -0400

    DB tables Migrated

--- Models/Answer.cs
+++ Models/Answer.cs
@@ -4,9 +4,10 @@ namespace suncoastoverflow.Models
 {
   public class Answer
   {
-public string AnswerBody { get; set; }
-public int AnswerVote { get; set; }
-public int? QuestionID { get; set; }
-public Question Question { get; set; }
+    public int ID { get; set; }
+    public string AnswerBody { get; set; }
+    public int AnswerVote { get; set; }
+    public int? QuestionID { get; set; }
+    public Question Question { get; set; }
   }
 }
--- Models/Question.cs
+++ Models/Question.cs
@@ -4,9 +4,10 @@ namespace suncoastoverflow.Models
 {
   public class Question
   {
-public string QuestionTitle { get; set; }
-public string QuestionBody { get; set; }
-public int QuestionVote { get; set; }
-public bool Answered { get; set; }
+    public int ID { get; set; }
+    public string QuestionTitle { get; set; }
+    public string QuestionBody { get; set; }
+    public int QuestionVote { get; set; }
+    public bool Answered { get; set; }
   }
 }
