e0d91e51b1c1c85b896ff5cfd1a3828a5865d9bb
hectorscreativeworld <hectorscreativeworld@gmail.com>
2019-06-25 21:25:27 -0400

    hardcoded data

--- ClientApp/src/components/Pages/Home.js
+++ ClientApp/src/components/Pages/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Questions from '../Questions'
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h1>Home Page</h1>
+        <Questions />
       </div>
     )
   }
--- ClientApp/src/components/Questions.js
+++ ClientApp/src/components/Questions.js
@@ -2,7 +2,39 @@ import React, { Component } from 'react'
 
 class Questions extends Component {
   render() {
-    return <div />
+    const questions = [
+      {
+        title:
+          'Foreign Key association with pre-exisitng table cant find table',
+        votes: 0,
+        answers: 0,
+        views: 1,
+        tags: ['python', 'Flask', 'c#']
+      },
+      {
+        title:
+          'How can I connect the ends of edges in order to close the hole between them?',
+        votes: 6,
+        answers: 2,
+        view: 55,
+        tags: ['python', 'HTML', 'F#']
+      }
+    ]
+    const allQuestions = questions.map(question => (
+      <div className="d-flex  Question-Container">
+        <div className="d-flex Vav-Container">
+          <div className="Vote-Container">{question.votes}</div>
+          <div className="Answer-Container">{question.answers}</div>
+          <div className="Views-Container">{question.views}</div>
+        </div>
+
+        <div className="Question-Title-Container">
+          <div className="Question-Title">{question.title}</div>
+        </div>
+      </div>
+    ))
+
+    return <div>{allQuestions}</div>
   }
 }
 
