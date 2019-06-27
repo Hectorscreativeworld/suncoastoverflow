b55fff8e9a1a47d5d4e2b5cec5fae271022d73c4
hectorscreativeworld <hectorscreativeworld@gmail.com>
2019-06-26 22:57:20 -0400

    askmeQuestion page

--- ClientApp/src/Pages/AnsweredHomePage.js
+++ ClientApp/src/Pages/AnsweredHomePage.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
+import Conversation from '../components/Conversation'
 
 class AnsweredHomePage extends Component {
   render() {
     return (
       <div>
-        </Conversation >
+        <Conversation />
         <span className="d-block p-2 bg-primary text-white">d-block</span>
 
         <div class="form-group">
-          <label for="comment">Comment:</label>
+          <label for="comment">Ask me Question:</label>
           <textarea class="form-control" rows="5" id="comment" />
         </div>
       </div>
--- ClientApp/src/Pages/AskAQuestionHomePage.js
+++ ClientApp/src/Pages/AskAQuestionHomePage.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import wiseMan from '../Images/wiseMan.png'
+import './AskAQuestionCss_Page.css'
 
 class AskAQuestionHomePage extends Component {
   render() {
+    console.log(wiseMan)
     return (
-      <div>
-        <h1>AskAQuestionHomePage</h1>
+      <div className="mainContainer">
+        <div className="form-group">
+          <label for="comment">Ask me question:</label>
+          <textarea className="form-control" rows="5" id="comment" />
+          <button type="button" className="btn btn-primary submitBtn">
+            submit
+          </button>
+        </div>
+
+        <div className="wiseMan">
+          <img src={wiseMan} alt="wise man" />
+        </div>
       </div>
     )
   }
