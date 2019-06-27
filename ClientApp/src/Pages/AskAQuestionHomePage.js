f633f336c679afd79867acaee93800fab1619a87
hectorscreativeworld <hectorscreativeworld@gmail.com>
2019-06-27 09:19:14 -0400

    Fixed ask Background

--- ClientApp/src/Pages/AskAQuestionHomePage.js
+++ ClientApp/src/Pages/AskAQuestionHomePage.js
@@ -7,16 +7,18 @@ class AskAQuestionHomePage extends Component {
   render() {
     console.log(oracal)
     return (
-      <div className="mainContainer">
-        <div className="form-group">
-          <label for="comment">Ask me question:</label>
-          <textarea className="form-control" rows="5" id="comment" />
-          <button type="button" className="btn btn-primary submitBtn">
-            submit
-          </button>
+      <section className="topContainer">
+        <div className="mainContainer">
+          <div className="form-group">
+            <label for="comment">Ask me question:</label>
+            <textarea className="form-control" rows="5" id="comment" />
+            <button type="button" className="btn btn-primary submitBtn">
+              submit
+            </button>
+          </div>
+          {/* <RandomTechNews /> */}
         </div>
-        {/* <RandomTechNews /> */}
-      </div>
+      </section>
     )
   }
 }
